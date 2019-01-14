97e1ab30a108479e14a571b91bb931481d1a5dbf
richardmaccaw <ricci6@gmail.com>
2019-01-14 17:20:36 +0000

    -configuring create user

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -10,9 +10,19 @@ class Api::V1::UsersController < ApplicationController
         render json: @user
     end
 
+    def create
+        @company = Company.find_or_create_by(domain: user_params[:domain])
+        @user = User.new(uid: user_params[:id], company_id: @company.id)
+        if @user.save
+            render json: @user
+        else
+            render json: {error: 'User could not be saved.'}
+        end
+    end
+
     private
 
     def user_params
-        params.require(:user).permit(:name, :id)
+        params.require(:user).permit(:name, :domain, :id)
     end
 end
