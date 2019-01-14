82731aee1b65b3e75a3338ada0501cf71e1de307
richardmaccaw <ricci6@gmail.com>
2019-01-14 12:30:29 +0000

    update method and delete method in announcements controller complete

--- app/controllers/api/v1/announcements_controller.rb
+++ app/controllers/api/v1/announcements_controller.rb
@@ -22,9 +22,27 @@ class Api::V1::AnnouncementsController < ApplicationController
         end
     end
 
+    def destroy
+        @announcement = Announcement.find_by(id: params[:id])
+        if @announcement.destroy
+            render json: @announcement
+        else
+            render json: {error: 'Unable to delete announcement'}, status: 400
+        end
+    end
+
+    def update
+        @announcement = Announcement.find_by(id: params[:id])
+        if @announcement.update(announcement_params)
+            render json: @announcement
+        else
+            render json: {error: 'Unable to update announcement'}, status: 400
+        end
+    end
+
     private
 
     def announcement_params
-        params.require(:announcement).permit(:title, :description, :date, :published, :company_id)
+        params.require(:announcement).permit(:title, :description, :date, :published, :company_id, :id)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users, only: [:index, :show]
-      resources :announcements, only: [:index, :show, :create]
+      resources :announcements, only: [:index, :show, :create, :destroy, :update]
     end
   end
 end
