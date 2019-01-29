b162ae8515c3042a6ccd9d5c05d2bb893a82784c
richardmaccaw <ricci6@gmail.com>
2019-01-29 12:31:03 +0000

    created onboarding announcement for new companies

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -10,8 +10,22 @@ class Api::V1::UsersController < ApplicationController
         render json: @user
     end
 
+    # def create
+    #     @company = Company.find_or_create_by(domain: params[:domain])
+    #     @user = User.find_or_create_by(uid: user_params[:uid], company_id: @company.id)
+    #     if @user.save
+    #         render json: @user
+    #     else
+    #         render json: {error: 'User could not be saved.'}
+    #     end
+    # end
+
     def create
-        @company = Company.find_or_create_by(domain: params[:domain])
+        @company = Company.find_by(domain: params[:domain])
+        if !@company
+            @company = Company.create(domain: params[:domain])
+            Announcement.createOnboardingAnnouncement(@company.id)
+        end
         @user = User.find_or_create_by(uid: user_params[:uid], company_id: @company.id)
         if @user.save
             render json: @user
@@ -25,7 +39,5 @@ class Api::V1::UsersController < ApplicationController
     def user_params
         params.require(:user).permit(:name, :uid, :domain)
     end
-
-# finish
 
 end
--- app/models/announcement.rb
+++ app/models/announcement.rb
@@ -1,4 +1,8 @@
 class Announcement < ApplicationRecord
     belongs_to :company
     has_many :users, through: :company
+
+    def self.createOnboardingAnnouncement(id)
+        Announcement.create(title: 'Welcome to CompanyTab!', description: 'test description', published: true, company_id: id)
+    end
 end
