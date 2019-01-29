578ee6fef8bac8a4b719affbc200643c2718ea61
richardmaccaw <ricci6@gmail.com>
2019-01-29 15:21:34 +0000

    updated typo in onboarding text

--- app/models/announcement.rb
+++ app/models/announcement.rb
@@ -5,7 +5,7 @@ class Announcement < ApplicationRecord
     def self.createOnboardingAnnouncement(id)
         Announcement.create(
             title: 'Welcome to CompanyTab! 🥳 ',
-            description: 'We think that sharing information should be easy. This new tab page will act as a hub everyone at your company. Here you and your co-workers can read your company announcements, office timezones and quick links. Head to the admin page to create your first announcement. Once you publish it, the announcement will spread to all your co-workers new tabs!',
+            description: 'We think that sharing information should be easy. This new tab page will act as a hub for everyone at your company. Here you and your co-workers can read your company announcements, office timezones and quick links. Head to the admin page to create your first announcement. Once you publish it, the announcement will spread to all your co-workers new tabs!',
             published: true, company_id: id
         )
     end
