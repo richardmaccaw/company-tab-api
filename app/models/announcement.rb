51aaf5872c8d37d56ed62d4fa77d61ce81058166
richardmaccaw <ricci6@gmail.com>
2019-01-29 14:17:33 +0000

    updated onboarding copy

--- app/models/announcement.rb
+++ app/models/announcement.rb
@@ -4,8 +4,8 @@ class Announcement < ApplicationRecord
 
     def self.createOnboardingAnnouncement(id)
         Announcement.create(
-            title: 'Welcome to CompanyTab! 🥳',
-            description: 'We believe that sharing information should be easy. This new tab page will act as a home page for everyone at your company. Here you and your co-workers can read (but not create) your company announcements, office timezones and quick links. Login to the web app (www.companytab.herokuapp.com) to create your first announcement. Once you publish it, the announcement will spread to all your co-workers new tabs!',
+            title: 'Welcome to CompanyTab! 🥳 ',
+            description: 'We think that sharing information should be easy. This new tab page will act as a hub everyone at your company. Here you and your co-workers can read your company announcements, office timezones and quick links. Head to the admin page to create your first announcement. Once you publish it, the announcement will spread to all your co-workers new tabs!',
             published: true, company_id: id
         )
     end
